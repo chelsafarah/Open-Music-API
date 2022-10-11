1716905ef48b3c24499fe618dabec73eed9d3a78
unknown <chelsafv75@gmail.com>
2022-10-11 19:08:36 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,39 @@
+const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const songs = require('./api/songs');
+const AlbumsService = require('./services/inMemory/AlbumsService');
+const SongsService = require('./services/inMemory/SongsService');
+
+const init = async () => {
+  const albumsService = new AlbumsService();
+  const songsService = new SongsService();
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register([
+    {
+      plugin: albums,
+      options: {
+        service: albumsService,
+      },
+    },
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+      },
+    },
+  ]);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
