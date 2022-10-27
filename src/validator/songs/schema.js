c7cdca0d7f995e2bf0fe096b9a28ef1670bdaa84
unknown <chelsafv75@gmail.com>
2022-10-27 23:24:06 +0700

    change schema

--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const SongPayloadSchema = Joi.object({
-  title: Joi.string().required(),
+  title: Joi.string().max(100).required(),
   year: Joi.number().required(),
   genre: Joi.string().required(),
   performer: Joi.string().required(),
